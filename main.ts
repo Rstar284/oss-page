1b2c844006d4ac8ab4a3db2e73b1e3ad6b1a098f
TruncatedDinosour <71613062+TruncatedDinosour@users.noreply.github.com>
2022-05-07 16:52:21 +0000

    Clean up your code (uwu)
    smh kids these days
    (*this is a joke but I did clean your code up uwu*)

--- main.ts
+++ main.ts
@@ -1,34 +1,38 @@
-import {
-  Application,
-  Context,
-  Router,
-  Status,
+impoimport {
+    Application,
+    Context,
+    Router,
 } from "https://deno.land/x/oak@v10.2.0/mod.ts";
 
 const app = new Application();
-
 const router = new Router();
-router
-  .get("/", (ctx: Context) => {
-    const agent = ctx.request.headers.get("user-agent");
-    if(!agent) {
-      return;
-    }
-    const split = agent.split(/[\s+|\/]/g)
-    if(split.includes("Firefox")) {
-      ctx.response.body = "Ooh you use firefox, or atleast appear to, (smh to those who changed their user-agent), anyway you deserve a cookie"
-      ctx.response.headers.set("Refresh", "4;https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    } else {
-      ctx.response.body = "Please for all purposes and reasons, switch to Firefox for your daily usage. As you are not currently using Firefox, we cannot serve you this page, for this server only serves OSS consumers"
-      ctx.response.headers.set("Refresh", "4;https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    }
-  })
 
-app.addEventListener("listen", ({ hostname, port }) => {
-  console.log(`Listening on: ${hostname}:${port}`);
-});
+router.get("/", (ctx: Context) => {
+    const agent = ctx.request.headers.get("user-agent");
+
+    if (!agent)
+        return;
+
+    const split = agent.split(/[\s+|\/]/g)
+
+    if (split.includes("Firefox")) ctx.response.body =
+        "Ooh you use firefox, or atleast appear to, \
+(smh to those who changed their user-agent), anyway you deserve a cookie"
+    else ctx.response.body =
+        "Please for all purposes and reasons, switch to Firefox for your \
+daily usage. As you are not currently using Firefox, we cannot serve you \
+this page, for this server only serves OSS consumers"
+
+    ctx.response.headers.set("Refresh",
+                             "4;https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+})
+
+app.addEventListener(
+    "listen",
+    ({hostname,
+      port}) => { console.log(`Listening on: ${hostname}:${port}`); });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen({ port: 8080 });
+await app.listen({port : 8080});
